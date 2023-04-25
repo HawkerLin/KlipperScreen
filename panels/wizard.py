5c94204e9e1148e873123d1776e731c442989591
HawkerLin <1037897430@qq.com>
2023-04-25 15:39:37 +0800

    dev

--- panels/wizard.py
+++ panels/wizard.py
@@ -45,7 +45,7 @@ class WizardPanel(ScreenPanel):
         self.wizard_2_lbl.set_label("Wizard")
         self.wizard_2_title = Gtk.Box()
         self.wizard_2_title.get_style_context().add_class("title_bar")
-        self.wizard_2_title.set_size_request(120, 350)
+        self.wizard_2_title.set_size_request(240, 700)
         self.wizard_2_title.set_valign(Gtk.Align.CENTER)
         self.wizard_2_title.add(self.wizard_2_lbl)
 
@@ -53,14 +53,14 @@ class WizardPanel(ScreenPanel):
         self.second_nex.connect("clicked", self.final_next)
         self.wizard_2_next = Gtk.Box()
         self.wizard_2_next.get_style_context().add_class('frame-item')
-        self.wizard_2_next.set_size_request(120, 50)
+        self.wizard_2_next.set_size_request(120, 100)
         self.wizard_2_next.add(self.second_nex)
 
-        self.second_bac = self._gtk.Button(label="End")
+        self.second_bac = self._gtk.Button(label="Back")
         self.second_bac.connect("clicked", self.second_back)
         self.wizard_2_back = Gtk.Box()
         self.wizard_2_back.get_style_context().add_class('frame-item')
-        self.wizard_2_back.set_size_request(120, 50)
+        self.wizard_2_back.set_size_request(120, 100)
         self.wizard_2_back.add(self.second_bac)
 
         self.wizard_page_2 = Gtk.Grid()
