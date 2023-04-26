2676f87c36dad937089a3900fc7e25bc31d4bbd5
HawkerLin <1037897430@qq.com>
2023-04-26 20:00:09 +0800

    dev

--- panels/wizard.py
+++ panels/wizard.py
@@ -91,7 +91,7 @@ class WizardPanel(ScreenPanel):
         #self.logo.pack_end(image, False, False, 10)#将image添加到self.logo的末尾位置
 
         self.second_nex = self._gtk.Button("arrow-right","Next", f"color2")
-        self.second_nex.connect("clicked", self.final_next)
+        self.second_nex.connect("clicked", self.second_next)
         self.second_nex.set_size_request(210, 35)
         self.second_nex.set_halign(Gtk.Align.CENTER)
         self.second_nex.set_valign(Gtk.Align.CENTER)
@@ -120,6 +120,58 @@ class WizardPanel(ScreenPanel):
         self._screen.add(self._screen.wizard.wizard_page_2)
         self._screen.show_all()
 
+    def show_wizard_3(self):
+        self.wizard_3_lbl = Gtk.Label()
+        self.wizard_3_lbl.set_hexpand(True)
+        self.wizard_3_lbl.set_halign(Gtk.Align.CENTER)
+        self.wizard_3_lbl.set_valign(Gtk.Align.CENTER)
+        #self.wizard_3_lbl.set_ellipsize(Pango.EllipsizeMode.END)
+        #self.wizard_3_lbl.set_label("Wizard")
+        self.wizard_3_lbl.set_markup("<span font='DejaVu Sans-bold 30'>Leveling Calibration</span>")
+        self.wizard_3_title = Gtk.Box()
+        #self.wizard_3_title.get_style_context().add_class("title_bar")
+        self.wizard_3_title.set_size_request(480, 80)
+        self.wizard_3_title.set_valign(Gtk.Align.START)
+        self.wizard_3_title.add(self.wizard_3_lbl)
+
+        image = self._gtk.Image("level", self._gtk.content_width * .5, self._gtk.content_height * .5)
+        self.level_logo = Gtk.Box()
+        #self.level_logo.set_halign(Gtk.Align.CENTER)
+        #self.level_logo.set_valign(Gtk.Align.CENTER)
+        self.level_logo.set_size_request(480, 640)
+        self.level_logo.pack_end(image, True, True, 20)#将image添加到self.logo的起始位置
+        #self.logo.pack_end(image, False, False, 10)#将image添加到self.logo的末尾位置
+
+        self.third_nex = self._gtk.Button("arrow-right","Next", f"color2")
+        self.third_nex.connect("clicked", self.final_next)
+        self.third_nex.set_size_request(210, 35)
+        self.third_nex.set_halign(Gtk.Align.CENTER)
+        self.third_nex.set_valign(Gtk.Align.CENTER)
+        self.wizard_3_next = Gtk.Box()
+        self.wizard_3_next.pack_start(self.third_nex, False, False, 10)
+        #self.wizard_3_next.get_style_context().add_class('button_active')
+        self.wizard_3_next.set_size_request(340, 80)
+        self.wizard_3_next.add(self.third_nex)
+
+        self.third_bac = self._gtk.Button("arrow-left","Back", f"color1")
+        self.third_bac.connect("clicked", self.third_back)
+        self.third_bac.set_size_request(210, 35)
+        self.third_bac.set_halign(Gtk.Align.CENTER)
+        self.third_bac.set_valign(Gtk.Align.CENTER)
+        self.wizard_3_back = Gtk.Box()
+        self.wizard_3_back.pack_start(self.third_bac, False, False, 10)
+        #self.wizard_3_back.get_style_context().add_class('button_active')
+        self.wizard_3_back.set_size_request(340, 80)
+        self.wizard_3_back.add(self.third_bac)
+
+        self.wizard_page_3 = Gtk.Grid()
+        self.wizard_page_3.attach(self.wizard_3_title, 0, 0, 2, 1)
+        self.wizard_page_3.attach(self.level_logo, 0, 1, 2, 1)
+        self.wizard_page_3.attach(self.wizard_3_back, 0, 3, 1, 1)
+        self.wizard_page_3.attach(self.wizard_3_next, 1, 3, 1, 1)
+        self._screen.add(self._screen.wizard.wizard_page_3)
+        self._screen.show_all()
+
     def first_next(self, widget):
         self._screen.remove(self.wizard_page_1)
         self.show_wizard_2()
@@ -129,6 +181,18 @@ class WizardPanel(ScreenPanel):
         self.show_wizard_1()
         self._screen.add(self._screen.wizard.wizard_page_1)
         self._screen.show_all()
+
+    def second_next(self,widget):
+        self._screen.remove(self.wizard_page_2)
+        self.show_wizard_3()
+        self._screen.add(self._screen.wizard.wizard_page_3)
+        self._screen.show_all()
+
+    def third_back(self,widget):
+        self._screen.remove(self.wizard_page_3)
+        self.show_wizard_3()
+        self._screen.add(self._screen.wizard.wizard_page_2)
+        self._screen.show_all()
         
 
     # def second_next(self, widget):
@@ -136,7 +200,7 @@ class WizardPanel(ScreenPanel):
 
     def final_next(self, widget):
         #self._screen.remove(self.wizard_page)#如果只是remove这个而没有再次add、show_all则是黑屏
-        self._screen.remove(self.wizard_page_2)
+        self._screen.remove(self.wizard_page_3)
         self._screen.add(self._screen.base_panel.main_grid)
         self._screen.show_all()
 
