33d7f51eb2678077f0f148aae216676d5f71a21c
HawkerLin <1037897430@qq.com>
2023-04-26 11:17:37 +0800

    dev

--- panels/wizard.py
+++ panels/wizard.py
@@ -17,13 +17,14 @@ class WizardPanel(ScreenPanel):
     def show_wizard_1(self):
         self.wizard_1_lbl = Gtk.Label()
         self.wizard_1_lbl.set_hexpand(True)#水平扩展填充
-        self.wizard_1_lbl.set_halign(Gtk.Align.CENTER)#水平对齐设置为中心
+        self.wizard_1_lbl.set_halign(Gtk.Align.RIGHT)#水平对齐,CENTER设置为中心
         self.wizard_1_lbl.set_ellipsize(Pango.EllipsizeMode.END)#文本末尾空间不够的话以省略号显示
-        self.wizard_1_lbl.set_label("Wizard")
+        #self.wizard_1_lbl.set_label("Wizard")
+        self.wizard_1_lbl.set_markup("<span font='Sans 12'>向导</span>")
         self.wizard_1_title = Gtk.Box()
         self.wizard_1_title.get_style_context().add_class("title_bar")
         self.wizard_1_title.set_size_request(240, 680)
-        self.wizard_1_title.set_valign(Gtk.Align.CENTER)
+        self.wizard_1_title.set_valign(Gtk.Align.START)
         self.wizard_1_title.add(self.wizard_1_lbl)
 
         self.first_nex = self._gtk.Button("arrow-right","Next", f"color3")
