fce1a5e3102c33c29c407da0d214769b85a36ee5
HawkerLin <1037897430@qq.com>
2023-04-26 16:00:27 +0800

    dev

--- panels/wizard.py
+++ panels/wizard.py
@@ -17,7 +17,7 @@ class WizardPanel(ScreenPanel):
     def show_wizard_1(self):
         image = self._gtk.Image("sovoler", self._gtk.content_width * .2, self._gtk.content_height * .5)
         self.logo = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
-        self.logo.set_size_request(80, 80)
+        #self.logo.set_size_request(80, 80)
         self.logo.pack_start(image, False, True, 8)
         self.logo.pack_end(image, True, True, 8)
 
@@ -30,7 +30,7 @@ class WizardPanel(ScreenPanel):
         self.wizard_1_lbl.set_markup("<span font='DejaVu Sans-bold 38'>Your Languages</span>")
         self.wizard_1_title = Gtk.Box()
         self.wizard_1_title.get_style_context().add_class("title_bar")
-        self.wizard_1_title.set_size_request(120, 80)
+        #self.wizard_1_title.set_size_request(120, 80)
         self.wizard_1_title.set_valign(Gtk.Align.START)
         self.wizard_1_title.add(self.wizard_1_lbl)
 
@@ -40,7 +40,7 @@ class WizardPanel(ScreenPanel):
         self.wizard_1_next.set_halign(Gtk.Align.CENTER)
         self.wizard_1_next.set_valign(Gtk.Align.CENTER)
         self.wizard_1_next.get_style_context().add_class('button_active')
-        self.wizard_1_next.set_size_request(470, 80)
+        #self.wizard_1_next.set_size_request(470, 80)
         self.wizard_1_next.add(self.first_nex)
 
 
@@ -49,7 +49,7 @@ class WizardPanel(ScreenPanel):
         self.language_menu.set_halign(Gtk.Align.CENTER)
         self.language_menu.set_valign(Gtk.Align.START)
         self.language = Gtk.Box()
-        self.language.set_size_request(470, 600)
+        #self.language.set_size_request(470, 600)
         self.language.add(self.language_menu)
 
         self.wizard_page_1 = Gtk.Grid()
@@ -67,7 +67,7 @@ class WizardPanel(ScreenPanel):
         self.wizard_2_lbl.set_label("Wizard")
         self.wizard_2_title = Gtk.Box()
         self.wizard_2_title.get_style_context().add_class("title_bar")
-        self.wizard_2_title.set_size_request(240, 680)
+        #self.wizard_2_title.set_size_request(240, 680)
         self.wizard_2_title.set_valign(Gtk.Align.CENTER)
         self.wizard_2_title.add(self.wizard_2_lbl)
 
@@ -75,14 +75,14 @@ class WizardPanel(ScreenPanel):
         self.second_nex.connect("clicked", self.final_next)
         self.wizard_2_next = Gtk.Box()
         self.wizard_2_next.get_style_context().add_class('button_active')
-        self.wizard_2_next.set_size_request(230, 80)
+        #self.wizard_2_next.set_size_request(230, 80)
         self.wizard_2_next.add(self.second_nex)
 
         self.second_bac = self._gtk.Button(label="Back")
         self.second_bac.connect("clicked", self.second_back)
         self.wizard_2_back = Gtk.Box()
         self.wizard_2_back.get_style_context().add_class('button_active')
-        self.wizard_2_back.set_size_request(230, 80)
+        #self.wizard_2_back.set_size_request(230, 80)
         self.wizard_2_back.add(self.second_bac)
 
         self.wizard_page_2 = Gtk.Grid()
