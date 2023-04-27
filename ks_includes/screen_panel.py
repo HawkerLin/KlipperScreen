5822ebb0cf057c926016cecd9f765d634bc8682b
HawkerLin <1037897430@qq.com>
2023-04-27 17:58:34 +0800

    debug

--- ks_includes/screen_panel.py
+++ ks_includes/screen_panel.py
@@ -92,7 +92,7 @@ class ScreenPanel:
         if tree_iter is not None:
             model = combo.get_model()
             value = model[tree_iter][1]
-            logging.debug(f"0:{model[tree_iter][0]}")
+            logging.debug(f"{combo.get_active_text()}")
             logging.debug(f"[{section}] {option} changed to {value}")
             self._config.set(section, option, value)
             self._config.save_user_config_options()
